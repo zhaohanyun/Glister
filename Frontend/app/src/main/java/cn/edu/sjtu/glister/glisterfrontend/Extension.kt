21e00f740f556a6e1ee7ed5725a49a1fbdbd614d
ezzhang02 <ezzhang@umich.edu>
2022-07-11 19:25:44 -0400

    Add the file for image edit in the front end

--- Frontend/app/src/main/java/cn/edu/sjtu/glister/glisterfrontend/Extension.kt
+++ Frontend/app/src/main/java/cn/edu/sjtu/glister/glisterfrontend/Extension.kt
@@ -1,8 +1,15 @@
 package cn.edu.sjtu.glister.glisterfrontend
 
 import android.content.Context
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 
 fun Context.toast(message: String, short: Boolean = true) {
     Toast.makeText(this, message, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
+}
+fun ImageView.display(uri: Uri) {
+    setImageURI(uri)
+    visibility = View.VISIBLE
 }
